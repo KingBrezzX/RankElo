da2a38276debaefc844dbab99dd898033e626054
KingBrezzz <bobnbb046@gmail.com>
2026-08-29 02:32:20 +0700

    Update CombatListener.java

--- src/main/java/net/rankelo/plugin/listeners/CombatListener.java
+++ src/main/java/net/rankelo/plugin/listeners/CombatListener.java
@@ -2,7 +2,10 @@ package net.rankelo.plugin.listeners;
 
 import net.rankelo.plugin.RankEloPlugin;
 import net.rankelo.plugin.managers.EloManager;
-import org.bukkit.ChatColor;
+import net.rankelo.plugin.managers.RankManager;
+import net.rankelo.plugin.utils.MessageUtil;
+import net.rankelo.plugin.utils.WorldUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,10 +15,12 @@ public class CombatListener implements Listener {
 
     private final RankEloPlugin plugin;
     private final EloManager eloManager;
+    private final RankManager rankManager;
 
     public CombatListener(RankEloPlugin plugin) {
         this.plugin = plugin;
         this.eloManager = plugin.getEloManager();
+        this.rankManager = new RankManager(plugin);
     }
 
     @EventHandler
@@ -27,29 +32,88 @@ public class CombatListener implements Listener {
             return;
         }
 
-        int killElo = plugin.getConfig().getInt("elo.kill", 100);
-        int deathElo = plugin.getConfig().getInt("elo.death", 100);
+        if (!WorldUtil.isEnabled(plugin, victim.getWorld())) {
+            return;
+        }
 
-        eloManager.addElo(killer, killElo);
-        eloManager.removeElo(victim, deathElo);
+        if (!WorldUtil.isEnabled(plugin, killer.getWorld())) {
+            return;
+        }
 
-        int killerElo = eloManager.getElo(killer);
-        int victimElo = eloManager.getElo(victim);
+        int killElo = plugin.getConfig().getInt("elo.kill", 25);
+        int deathElo = plugin.getConfig().getInt("elo.death", 15);
+
+        int killerBefore = eloManager.getElo(killer.getUniqueId());
+        int victimBefore = eloManager.getElo(victim.getUniqueId());
+
+        String killerOldRank = rankManager.getRank(killerBefore);
+        String victimOldRank = rankManager.getRank(victimBefore);
+
+        eloManager.addElo(killer.getUniqueId(), killElo);
+        eloManager.removeElo(victim.getUniqueId(), deathElo);
+
+        int killerAfter = eloManager.getElo(killer.getUniqueId());
+        int victimAfter = eloManager.getElo(victim.getUniqueId());
+
+        String killerNewRank = rankManager.getRank(killerAfter);
+        String victimNewRank = rankManager.getRank(victimAfter);
 
         killer.sendMessage(
-                ChatColor.GREEN + "+" + killElo + " Elo "
-                        + ChatColor.GRAY + "for killing "
-                        + ChatColor.WHITE + victim.getName()
-                        + ChatColor.GRAY + ". Your Elo: "
-                        + ChatColor.AQUA + killerElo
+                MessageUtil.format(
+                        MessageUtil.get(plugin, "messages.kill"),
+                        "amount", killElo,
+                        "player", victim.getName(),
+                        "elo", killerAfter
+                )
         );
 
         victim.sendMessage(
-                ChatColor.RED + "-" + deathElo + " Elo "
-                        + ChatColor.GRAY + "because you were killed by "
-                        + ChatColor.WHITE + killer.getName()
-                        + ChatColor.GRAY + ". Your Elo: "
-                        + ChatColor.AQUA + victimElo
+                MessageUtil.format(
+                        MessageUtil.get(plugin, "messages.death"),
+                        "amount", deathElo,
+                        "player", killer.getName(),
+                        "elo", victimAfter
+                )
         );
+
+        if (!killerOldRank.equals(killerNewRank)
+                && plugin.getConfig().getBoolean(
+                "messages.promotion" != null
+                        ? "broadcast.promotion"
+                        : "broadcast.promotion",
+                true)) {
+
+            Bukkit.broadcastMessage(
+                    MessageUtil.format(
+                            MessageUtil.get(
+                                    plugin,
+                                    "messages.promotion"
+                            ),
+                            "player", killer.getName(),
+                            "rank_color",
+                            rankManager.getRankColor(killerNewRank),
+                            "rank", killerNewRank
+                    )
+            );
+        }
+
+        if (!victimOldRank.equals(victimNewRank)
+                && plugin.getConfig().getBoolean(
+                "broadcast.demotion",
+                true)) {
+
+            Bukkit.broadcastMessage(
+                    MessageUtil.format(
+                            MessageUtil.get(
+                                    plugin,
+                                    "messages.demotion"
+                            ),
+                            "player", victim.getName(),
+                            "rank_color",
+                            rankManager.getRankColor(victimNewRank),
+                            "rank", victimNewRank
+                    )
+            );
+        }
     }
-}
+                                                     }
